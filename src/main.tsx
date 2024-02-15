add815adce315cad57aeed53f7293a8b8f5a6f5c
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-15 23:54:57 +0530

    Add routing config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from "./components/app/App";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Authenticate from "./components/authenticate/Authenticate";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "authenticate",
+        element: <Authenticate />,
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+          { path: "register", element: <Register /> },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
