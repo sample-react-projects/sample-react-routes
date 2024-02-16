e14f5eae4c13a038c9a6229f0f769f6c2805293a
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-16 14:46:19 +0530

    Add logic to redirect to authenticate

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../ui/button/Button";
 import styles from "./Header.module.scss";
 
 const Header: React.FC<{}> = () => {
-  
+  const isAuthenticated = false;
+  const navigate = useNavigate();
+
+  function handleAuthenticationClick() {
+    if (!isAuthenticated) {
+      navigate("authenticate/login");
+    }
+
+    // TODO: Handle logout
+  }
 
   return (
     <header className={styles.header}>
       <strong>Sample react routes</strong>
-      <div className="navigation">
-        Navigation
+      <div className={styles.authentication}>
+        <Button type="button" onClick={handleAuthenticationClick}>
+          {isAuthenticated ? "Logout" : "Login"}
+        </Button>
       </div>
     </header>
   );
