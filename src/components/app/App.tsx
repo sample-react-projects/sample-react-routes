9bc9a244588c724e0d33a009a0b634e4401adada
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-16 17:44:15 +0530

    structural changes to use card component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
-import "./App.module.scss";
+import styles from "./App.module.scss";
 import Header from "../header/Header";
+import Card from "../ui/card/Card";
 
 function App() {
   return (
     <>
-      <Header />
-      <Outlet />
+      <div className={styles.header}>
+        <div className={styles.header__content}>
+          <Header />
+        </div>
+      </div>
+      <div className={styles.card__content}>
+        <Card>
+          <Outlet />
+        </Card>
+      </div>
     </>
   );
 }
