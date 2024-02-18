86cba3b08804925223d842bdecf9a07be4a4a92a
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-18 18:10:59 +0530

    Use constant key

--- src/util/localStorage.ts
+++ src/util/localStorage.ts
@@ -1,7 +1,9 @@
+export const AUTH_TOKEN_KEY = "authToken";
+
 export function getAuthToken() {
-  return localStorage.getItem("authToken");
+  return localStorage.getItem(AUTH_TOKEN_KEY);
 }
 
 export function setAuthToken(token: string) {
-  return localStorage.setItem("authToken", token);
+  return localStorage.setItem(AUTH_TOKEN_KEY, token);
 }
