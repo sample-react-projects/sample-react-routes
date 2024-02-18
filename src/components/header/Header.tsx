31e33d46093b68b4b5e2fdc8d82f263ba07b28f0
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-18 18:35:05 +0530

    handle logout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,14 @@
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Form, Link, NavLink, useNavigate } from "react-router-dom";
 import Button from "../ui/button/Button";
 import styles from "./Header.module.scss";
+import { getAuthToken } from "../../util/localStorage";
 
 const Header: React.FC<{}> = () => {
-  const isAuthenticated = false;
+  const isAuthenticated = !!getAuthToken();
   const navigate = useNavigate();
 
-  function handleAuthenticationClick() {
-    if (!isAuthenticated) {
-      navigate("authenticate/login");
-    }
-
-    // TODO: Handle logout
+  function handleLoginClick() {
+    navigate("authenticate/login");
   }
 
   return (
@@ -20,21 +17,28 @@ const Header: React.FC<{}> = () => {
         <strong>Sample react routes</strong>
       </Link>
       <div className={styles.right}>
-        {!isAuthenticated && (
-          <NavLink
-            to="/products"
-            className={({ isActive }) =>
-              isActive
-                ? `${styles.navlink} ${styles["navlink--active"]}`
-                : styles.navlink
-            }
-          >
-            Products
-          </NavLink>
+        {isAuthenticated ? (
+          <>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles.navlink} ${styles["navlink--active"]}`
+                  : styles.navlink
+              }
+            >
+              Products
+            </NavLink>
+
+            <Form action="/logout" method="POST">
+              <Button>Logout</Button>
+            </Form>
+          </>
+        ) : (
+          <Button type="button" onClick={handleLoginClick}>
+            Login
+          </Button>
         )}
-        <Button type="button" onClick={handleAuthenticationClick}>
-          {isAuthenticated ? "Logout" : "Login"}
-        </Button>
       </div>
     </header>
   );
