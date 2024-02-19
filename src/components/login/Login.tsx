26f9b3235ab8a2ad8f9dc460bc63230ba08d6aeb
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-19 12:52:54 +0530

    render action data

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,28 +1,33 @@
-import { Form } from "react-router-dom";
+import { Form, useActionData } from "react-router-dom";
 import styles from "./Login.module.scss";
 import Button from "../ui/button/Button";
 
 const Login: React.FC<{}> = () => {
+  const actionData = useActionData() as { error: string };
+
   return (
-    <Form action="/authenticate/login" method="POST" className={styles.form}>
-      <input
-        type="text"
-        name="username"
-        id="username"
-        placeholder="username"
-        autoComplete="off"
-      />
-      <input
-        type="password"
-        name="password"
-        id="password"
-        placeholder="password"
-        autoComplete="off"
-      />
-      <div className={styles.actions}>
-        <Button>Login</Button>
-      </div>
-    </Form>
+    <>
+      {actionData && actionData.error ? <p>{actionData.error}</p> : null}
+      <Form action="/authenticate/login" method="post" className={styles.form}>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="username"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          autoComplete="off"
+        />
+        <div className={styles.actions}>
+          <Button>Login</Button>
+        </div>
+      </Form>
+    </>
   );
 };
 
