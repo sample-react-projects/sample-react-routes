3c795a5020772a14f078986ce86f5e04cf849dca
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-19 08:23:17 +0530

    Add login form

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,29 @@
+import { Form } from "react-router-dom";
+import styles from "./Login.module.scss";
+import Button from "../ui/button/Button";
+
 const Login: React.FC<{}> = () => {
-  return <></>;
+  return (
+    <Form action="/authenticate/login" method="POST" className={styles.form}>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        placeholder="username"
+        autoComplete="off"
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="password"
+        autoComplete="off"
+      />
+      <div className={styles.actions}>
+        <Button>Login</Button>
+      </div>
+    </Form>
+  );
 };
 
 export default Login;
