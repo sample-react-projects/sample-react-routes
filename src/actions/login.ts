7932faab70bd179833ad3eaddace94e7b55420d9
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-19 12:52:36 +0530

    Update login action

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -3,17 +3,33 @@ import { setAuthToken } from "../util/localStorage";
 
 export const loginAction: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
-  const username = formData.get("username");
-  const password = formData.get("password");
+  const username = formData.get("username")?.toString() || "";
+  const password = formData.get("password")?.toString() || "";
 
-  if (username !== password) {
-    return new Response(new Blob(), {
-      status: 244,
-      statusText: "Invalid credentials",
-    });
+  if (!username || !password) {
+    return { error: "Empty credentials!" };
   }
 
-  setAuthToken(`${username}#${password}`);
-
-  return redirect("/");
+  try {
+    let response = await initiateLogin(username, password);
+    setAuthToken(response);
+    return redirect("/");
+  } catch {
+    return { error: "Invalid credentials!" };
+  }
 };
+
+async function initiateLogin(
+  username: string,
+  password: string
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (username !== password) {
+        reject("Invalid credentials");
+      } else {
+        resolve(`${username}#${password}`);
+      }
+    }, 2000);
+  });
+}
